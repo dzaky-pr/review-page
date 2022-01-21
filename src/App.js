f390b96e8c301591f8f79f8cffb5b85f46ba2902
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2022-01-21 20:40:47 +0700

    fix typo
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Review from './Review';
 function App() {
   return (
     <main>
-      <section className="cointainer">
+      <section className="container">
         <div className="title">
           <h2>Our Reviews</h2>
           <div className="underline"></div>
